82a6c239d15217b9281ea5ae87c9c55f65cb1217
Devian Parra <devianparrapadilla@gmail.com>
2019-11-09 09:00:19 -0500

    Update and rename code_framingham to code_framingham.R

--- code_framingham.R
+++ code_framingham.R
@@ -1,16 +1,16 @@
-#####Framingham Risk Score Calculation
+#Framingham Risk Score Calculation
 
 Here I create a database of registries from the "datos_dtc" longitudinal database where each row is a visit of each patient to 
 health care providers to recieve interventions within the program activities. I filter registries by date and select relevant clinical 
 variables
 
-```Ruby
+
 controles_2018<-datos_dtc %>% filter(fecha_control_o_de_atencion_ >= "2018-01-01") %>%  
   select(id, fecha_control_o_de_atencion_, tas, tad, hba1c, erc, dm, hta, disl, col_ldl, col_hdl, 
   col_total, tfg, afic_sexo, afif_nacimiento, peso, talla, nom_dpto)
-```
 
-#Se obtiene la última obserbación para cada variable en la base
+
+# Se obtiene la última obserbación para cada variable en la base
 f_ult<-function(x){
   df<- controles_2018[c("id", "fecha_control_o_de_atencion_", x)]
   df <- df[!is.na(df[x]), ]
