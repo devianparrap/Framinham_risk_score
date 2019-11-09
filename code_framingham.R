eae97eafa5118560b63253d22e4037381c7a651d
Devian Parra <devianparrapadilla@gmail.com>
2019-11-09 09:49:46 -0500

    Update code_framingham.R

--- code_framingham.R
+++ code_framingham.R
@@ -1,20 +1,22 @@
 #Framingham Risk Score Calculation
 
-#Here I create a database of registries from the "datos_dtc" longitudinal database where each row is a visit of each patient to 
+#I create a database of registries from the "datos_dtc" longitudinal database where each row is a visit of each patient to 
 #health care providers to recieve interventions within the program activities. I filter registries by date and select relevant clinical 
-#variables
-
+#variables. This results in a database of all controls per each patient in 2018
 
 controles_2018<-datos_dtc %>% filter(fecha_control_o_de_atencion_ >= "2018-01-01") %>%  
   select(id, fecha_control_o_de_atencion_, tas, tad, hba1c, erc, dm, hta, disl, col_ldl, col_hdl, 
   col_total, tfg, afic_sexo, afif_nacimiento, peso, talla, nom_dpto)
 
 
-# Se obtiene la última obserbación para cada variable en la base
+# A funtion was created to select the latest observation of each variable within the 
+# database created in the step before
+
 f_ult<-function(x){
   df<- controles_2018[c("id", "fecha_control_o_de_atencion_", x)]
   df <- df[!is.na(df[x]), ]
-  df %>% arrange(fecha_control_o_de_atencion_) %>% group_by(id) %>% filter_all(all_vars(!is.na(.))) %>% filter(row_number() == n()) %>% 
+  df %>% arrange(fecha_control_o_de_atencion_) %>% group_by(id) %>% 
+  filter_all(all_vars(!is.na(.))) %>% filter(row_number() == n()) %>% 
     select(-fecha_control_o_de_atencion_)
 } 
 
@@ -26,12 +28,20 @@ for (i in 1:length(list_var)) {
   controles_2018<-merge(controles_2018, list_var[[i]], by= "id", all.x = T)  
 }
 
-#Se crea la variable edad en la base 
+# A age variable was created by selecting the id and birth date of each patient in the original database 
+# and merged to the resulting database of the prevoius step
 
 f_control<-datos_dtc %>% filter(fecha_control_o_de_atencion_ >= "2018-01-01") %>%  
-  select(id, fecha_control_o_de_atencion_) %>%  group_by(id) %>% arrange(fecha_control_o_de_atencion_) %>% filter(row_number() == n()) %>% distinct()
+  select(id, fecha_control_o_de_atencion_) %>%  group_by(id) %>% arrange(fecha_control_o_de_atencion_) %>% 
+filter(row_number() == n()) %>% distinct()
 
 controles_2018<- merge(controles_2018, f_control, by = "id") 
 controles_2018<-controles_2018 %>% group_by(id) %>% 
   mutate(edad_afil = floor(as.numeric((fecha_control_o_de_atencion_ - afif_nacimiento)/365.25, units = "days"))) %>%
-  filter(!nom_dpto %in% c("BOGOTÁ, D. C.", "SANTANDER", "CASANARE", "PUTUMAYO", "ANTIOQUIA", "NARIÑO", "LA GUAJIRA", "BOYACÁ", "CUNDINAMARCA", ""))
+  filter(!nom_dpto %in% c("BOGOTÁ, D. C.", "SANTANDER", "CASANARE", "PUTUMAYO", "ANTIOQUIA", "NARIÑO", 
+                          "LA GUAJIRA", "BOYACÁ", "CUNDINAMARCA", ""))
+
+
+
+
+
